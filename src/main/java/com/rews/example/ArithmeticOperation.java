6bfd02a2845b1a2018255d450878c4674d23ea74
nikitakakra <35792454+nikitakakra@users.noreply.github.com>
2020-01-26 13:17:57 +0530

    Update ArithmeticOperation.java

--- src/main/java/com/rews/example/ArithmeticOperation.java
+++ src/main/java/com/rews/example/ArithmeticOperation.java
@@ -5,5 +5,13 @@ public class ArithmeticOperation {
 	public Integer add(int num1 ,int num2) {
 		return num1+ num2;
 	}
+	
+	public Integer sub(int num1,int num2) {
+		return num1-num2;
+	}
+	
+	public Integer multiply(int num1,int num2) {
+		return num1*num2;
+	}
 
 }
